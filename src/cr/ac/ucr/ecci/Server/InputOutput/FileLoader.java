435fac0f13896d0d5501b6da03588af9de8b93e4
Elías Calderón <eliascr98@outlook.com>
2018-04-09 18:57:41 -0600

    File Loader
    Removed the temporal img fix

--- src/cr/ac/ucr/ecci/Server/InputOutput/FileLoader.java
+++ src/cr/ac/ucr/ecci/Server/InputOutput/FileLoader.java
@@ -1,8 +1,5 @@
 package cr.ac.ucr.ecci.Server.InputOutput;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -11,29 +8,13 @@ public class FileLoader {
 
     //TODO Hacer que haga esto pero para más tipo de media
     public static byte[] getFile (String filename) {
-        if (filename.contains(".png")) {
 
-            try {
-                BufferedImage img = ImageIO.read(new File(filename));
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                ImageIO.write(img, "png", baos);
-                baos.flush();
-                byte[] imageInByte = baos.toByteArray();
-                baos.close();
-                return imageInByte;
-            } catch (IOException e) {
-                return null;
-            }
+        File file = new File(filename);
 
-        } else {
-
-            File file = new File(filename);
-
-            try {
-                return Files.readAllBytes(file.toPath());
-            } catch (IOException e) {
-                return null;
-            }
+        try {
+            return Files.readAllBytes(file.toPath());
+        } catch (IOException e) {
+            return null;
         }
     }
 
